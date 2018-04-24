3f0d3ed601300f99ee93a24b03e11183436d4e3a
Vinay Acharya <vinay.r.acharya@gmail.com>
2018-04-24 03:16:47 -0500

    Done10

--- static/js/common.js
+++ static/js/common.js
@@ -29,6 +29,74 @@ function runOnLoad(){
 			$('#signupHorizonRow').hide();
 		}
 	});
+
+
+	initializeCarousel();
+	$('#showNextSpan').click(moveCarouselNext);
+	$('#showPreviousSpan').click(moveCarouselPrevious);
+}
+
+var previousCarouselIndex = 8
+var currentCarouselIndex = 0;
+var leftPosition = -250;
+var centerPosition = 100;
+var rightPosition = 750;
+var animationSpeed = 500
+function initializeCarousel(){
+	//$('#step'+0).show();
+	$('#step'+8).css('left', leftPosition);
+	$('#step'+0).css('left', centerPosition);
+	$('#step'+0).css('opacity', 1);
+	$('.crumbContainer:nth-child('+(currentCarouselIndex+2)+')').children('.crumb').addClass('active');	
+}
+
+function moveCarouselNext(){
+	//Move current center label to the left
+	$('#step'+currentCarouselIndex).animate({'left':leftPosition, 'opacity':0},animationSpeed);
+	
+	//Move the previous left label to the right
+	$('#step'+previousCarouselIndex).css('left', rightPosition);
+
+	//Change the previous index to the current index
+	previousCarouselIndex = currentCarouselIndex;
+
+	//Change the current index to the next index i.e. increament if <> 8, make -0 if =8
+	if(currentCarouselIndex==8){
+		currentCarouselIndex=0;
+	}
+	else{
+		currentCarouselIndex++;	
+	}
+
+	//Move next label to the center
+	$('#step'+(currentCarouselIndex)).animate({'left':centerPosition, 'opacity':1},animationSpeed);
+	$('.crumb').removeClass('active');
+	$('.crumbContainer:nth-child('+(currentCarouselIndex+2)+')').children('.crumb').addClass('active');
+}
+
+function moveCarouselPrevious(){
+
+	//Move current center label to the right
+	$('#step'+currentCarouselIndex).animate({'left':rightPosition, 'opacity':0},animationSpeed);
+	
+	//Move the previous left label to the center
+	$('#step'+previousCarouselIndex).animate({'left':centerPosition, 'opacity':100},animationSpeed);
+
+	//Change the current index to the previous index
+	currentCarouselIndex = previousCarouselIndex;
+
+	//Change the previous index to the one before it i.e. decreament if <> 0, make 8 if =0
+	if(previousCarouselIndex==0){
+		previousCarouselIndex=8;
+	}
+	else{
+		previousCarouselIndex--;	
+	}
+
+	//Move the previous label to the left
+	$('#step'+(previousCarouselIndex)).css('left',leftPosition);
+	$('.crumb').removeClass('active');
+	$('.crumbContainer:nth-child('+(currentCarouselIndex+2)+')').children('.crumb').addClass('active');
 }
 
 function onSignUp(){
